cec3fa279925bae90cc3d83ba048ecf81d702b6b
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-09-09 03:43:51 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
-*main-Prints out the last part of a quote in the standard error.
-*Return:1 if success.
-*/
+ * main-prints out the last of a quote in the standard error.
+ * Return: 1 if success.
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
