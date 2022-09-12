1b6287c45dfa88e1c4b815fec7a9b83978d2b6a2
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-09-12 20:42:00 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,11 @@ int main(void)
 char low, e, q;
 e = 'e';
 q = 'q';
-for(low = 'a'; low <= 'z'; low++)
+for (low = 'a'; low <= 'z'; low++)
 {
-if(low != e && low != q)
+if (low != e && low != q)
 putchar(low);
 }
 putchar('\n');
-return(0);
+return (0);
 }
