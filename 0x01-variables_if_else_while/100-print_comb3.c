073ad33e8cc57f1ba453e4ed23caa457a2d473be
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-09-12 22:17:26 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 int d, p;
-for (d ='0'; d < '9'; p++)
+for (d = '0'; d < '9'; p++)
 {
-for(p = d + 1; p <= '9'; p++)
+for (p = d + 1; p <= '9'; p++)
 {
 if(p != d)
 {
@@ -24,4 +24,6 @@ putchar(' ');
 }
 }
 }
+putchar('\n');
+return (0);
 }
