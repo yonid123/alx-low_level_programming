7b5df64807c86c6f7791e8f6963c8f7a08a117bb
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-09-19 21:51:00 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,15 +3,13 @@
  * print_rev - Prints a string in reverse.
  * @s: string to be reserved.
  */
-void print_rev(char *s)
+void print_rev(int *a, int n)
 {
-	int len = 0, i;
-
-	while (s[i++])
-		len++;
-
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
-
-	_putchar('\n');
+int i, h;
+for (i = 0; i < (n / 2); i++)
+{
+h = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = h;
 }
+}	
