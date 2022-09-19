c116885832d1a55d38deab5feb0f8265e688f82d
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-09-19 22:22:25 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * puts2 - Prints one char out of two of a string.
@@ -5,10 +6,12 @@
  */
 void puts2(char *str)
 {
-int i, len = 0;
-while (str[i++])
-len++;
-for (i = 0; i < len; i += 2)
-_putchar(str[i]);
-_putchar('\n');
+int i = 0;
+while (*(str + i) != '\0')
+{
+if (i % 2 == 0)
+_putchar(*(str + i));
+i++;
+}
+_putchar(10);
 }
