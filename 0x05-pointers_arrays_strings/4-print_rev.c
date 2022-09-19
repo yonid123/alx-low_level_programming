f14d77cd72fd4142766a2c075a93058c7228ed25
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-09-19 21:44:08 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,10 +5,13 @@
  */
 void print_rev(char *s)
 {
-int len = 0, i;
-while (s[i++])
-len++;
-for (i = len - 1; i >= 0; i--)
-_putchar(s[i]);
-_putchar('\n');
+	int len = 0, i;
+
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
 }
