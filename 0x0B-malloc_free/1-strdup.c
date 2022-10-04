bd0d0fda4de071176da42009a0e5de2a8b5de9d6
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-10-04 22:16:31 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strdup - create a new array containing a copy of the given string
  * @str: a pointer to the string to copy
@@ -8,19 +9,22 @@
  */
 char *_strdup(char *str)
 {
-char *dup;
-unsigned int size = 0;
-if (str)
-{
-while (str[size++])
-;
-dup = malloc(sizeof(char) * size);
-if (dup)
-{
-while (size--)
-dup[size] = str[size];
-return (dup); 
-} 
-}
-return (NULL);
+	char *dup;
+	unsigned int size = 0;
+
+	if (str)
+	{
+		while (str[size++])
+			;
+
+		dup = malloc(sizeof(char) * size);
+		if (dup)
+		{
+			while (size--)
+				dup[size] = str[size];
+
+			return (dup);
+		}
+	}
+	return (NULL);
 }
