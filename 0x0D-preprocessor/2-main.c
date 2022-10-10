00ab8aa51997d3ef17a5f105d0cad00ab95b9ac2
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-10-10 12:10:03 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
   * main - Prints the file name of the program
   *
   * Return: Always zero
   */
-int main()
+int main(void)
 {
-printf("%s\n", __FILE__);
-return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
