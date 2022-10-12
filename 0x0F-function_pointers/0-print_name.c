27811c4efe0d22683a7f4b0dc242f676b52b1d6c
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-10-12 14:27:16 +0300

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints the name
+ * @name: name
+ * @f: function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+  if (name == NULL || f == NULL)
+    return;
+  f(name);
+}
