58ce80361314532169772751b6dfb594f2c596a1
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-10-12 15:26:07 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,32 +14,35 @@
  */
 int main(int argc, char *argv[])
 {
-int one, two, ans;
-int (*res)(int, int);
-char *get_op;
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
+	int one, two, ans;
+	int (*res)(int, int);
+	char *get_op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	one = atoi(argv[1]);
+	two = atoi(argv[3]);
+	get_op = argv[2];
+
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = get_op_func(get_op);
+	ans = res(one, two);
+
+	printf("%d\n", ans);
+	return (0);
 }
-one = atoi(argv[1]);
-two = atoi(argv[3]);
-get_op = argv[2];
-if (get_op_func(argv[2] == NULL || argv[2][1] != '\0')
-{
-printf("Error\n");
-exit(99);
-}
-if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
-{
-printf("Error\n");
-exit(100);
-}
-res = get_op_func(get_op);
-ans = res(one, two);
-printf("%d\n", ans);
-return (0);
-}
-      
-      
- 
