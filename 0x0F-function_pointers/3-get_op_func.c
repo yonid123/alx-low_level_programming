cd1fcaf6f07a14a5c0996f7af74eca6be95c5a28
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-10-12 15:29:22 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,20 +8,22 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-op_t op_s[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
-while (op_s[i].op)
-{
-if (*(op_s[i].op == *s)
-return (op_s[i].f);
-i++;
+	op_t op_s[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (op_s[i].op)
+	{
+		if (*(op_s[i].op) == *s)
+			return (op_s[i].f);
+		i++;
+	}
+	return (NULL);
 }
-return (NULL);
-}   
