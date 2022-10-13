7b49e1da69ec8625489cd201ec9e0fc1e86099f6
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-10-13 13:34:00 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "variadic_functions.h"
-#include <stdarg.h
+#include <stdarg.h>
 /**
  * sum_them_all - check the code for Holberton School students.
  * @n: number of arguments
@@ -11,7 +11,7 @@ int sum_them_all(const unsigned int n, ...)
 va_list valist;
 int sum = 0;
 unsigned int i;
-va_start(valist);
+va_start(valist, n);
 for (i = 0; i < n; i++)
 {
 sum += va_arg(valist, int);
