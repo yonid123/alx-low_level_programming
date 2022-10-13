bf03fa2d573202805d2e0a56db8d5477fb9e2aa8
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-10-13 14:31:22 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 #include <stdarg.h>
+
 /**
  * op_c - Print character .
  * @form: name va_list
  *
  * Return: Nothing.
  */
+
 void op_c(va_list form)
 {
-printf("%c", va_arg(form, int));
+	printf("%c", va_arg(form, int));
 }
 /**
  * op_i - Print Integer
@@ -17,9 +19,10 @@ printf("%c", va_arg(form, int));
  *
  * Return: Nothing.
  */
+
 void op_i(va_list form)
 {
-printf("%i", va_arg(form, int));
+	printf("%i", va_arg(form, int));
 }
 /**
  * op_f - print FLoat numbers
@@ -27,9 +30,10 @@ printf("%i", va_arg(form, int));
  *
  * Return: Nothing.
  */
+
 void op_f(va_list form)
 {
-printf("%f", va_arg(form, double));
+	printf("%f", va_arg(form, double));
 }
 /**
  * op_s -print string
@@ -37,55 +41,60 @@ printf("%f", va_arg(form, double));
  *
  * Return: Nothing.
  */
+
 void op_s(va_list form)
 {
-char *str;
-str = va_arg(form, char*);
-if (str == NULL)
-{
-printf("(nil)");
-return;
+	char *str;
+
+	str = va_arg(form, char *);
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
 }
-printf("%s", str);
-}
+
 /**
  * print_all - check the code for Holberton School students.
  * @format: number of arguments in character format
  *
  * Return: Nothing.
  */
+
 void print_all(const char * const format, ...)
 {
-va_list all;
-unsigned int i, j;
-char *separator = "";
-f ops[] = {
-{"c", op_c},
-{"i", op_i},
-{"f", op_f},
-{"s", op_s},
-};
-va_start(all, format);
-i = 0;
-while (format && format[i])
-{
-j = 0;
-while (j < 4)
-{
-if (ops[j].op[0] == format[i])
-{
-printf("%s", separator);
-separator = ", ";
-ops[j].f(all);
-break;
+
+	va_list all;
+	unsigned int i, j;
+	char *separator = "";
+
+	f ops[] = {
+		{"c", op_c},
+		{"i", op_i},
+		{"f", op_f},
+		{"s", op_s},
+		};
+
+	va_start(all, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (ops[j].op[0] == format[i])
+			{
+				printf("%s", separator);
+				separator = ", ";
+				ops[j].f(all);
+				break;
+			}
+			j++;
+		}
+	i++;
+	}
+
+	printf("\n");
+	va_end(all);
 }
-j++;
-}
-i++;
-}
-printf("\n");
-va_end(all);
-}
-    
-    
-  
