220a7ea3d07bb9271ddab981c6c3dc0bf89408d4
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-10-25 13:26:26 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,8 +2,10 @@
  * File: 1-listint_len.c
  * Auth: Brennan D Baraban
  */
+
 #include "lists.h"
 #include <stdio.h>
+
 /**
  * listint_len - Returns the number of elements
  *               in a linked listint_t list.
@@ -13,11 +15,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-size_t nodes = 0;
-while(h)
-{
-nodes++;
-h = h->next;
-}
-return (nodes);
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
