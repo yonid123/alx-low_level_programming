86796cad351ca48306a4f5a60f0b78519bd45e34
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-10-27 17:47:27 +0300

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_binary(unsigned long int n)
+{
+ if (n > 1)
+ print_binary(n >> 1);
+ _putchar((n & 1) + '0');
+}
