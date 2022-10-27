7f158293a2fd799213c69abadc93625ed400bb55
yonid123 <111308870+yonid123@users.noreply.github.com>
2022-10-27 17:13:09 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,17 +10,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int value = 0;
-  if (b == NULL)
-   return (0);
-  while (*b != '\0')
-  {
-  value = value << 1;
-  if (*b != '1' && *b != '0')
-   return (0);
-  else if (*b == '1')
-  value = value | 1;
-  b++;
-  }
-  return (value);
+	unsigned int value = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		value = value << 1;
+		if (*b != '1' && *b != '0')
+			return (0);
+		else if (*b == '1')
+			value = value | 1;
+		b++;
+	}
+	return (value);
 }
